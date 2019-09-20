51ba7c51459307bd5f2067940ef93415f3559b9f
korea8378 <korea8378@naver.com>
2019-09-20 19:00:17 +0900

    [MASTER][ADD] 5.람다로 프로그래밍(5.1까지)

---
+++ src/chapter5/LambdaExpressions.kt
@@ -0,0 +1,21 @@
+package chapter5
+
+fun main(args: Array<String>) {
+    val sum = { x: Int, y: Int ->
+        println("Computing the sum of $x and $y...")
+        x + y
+    }
+    println(sum(1, 2))
+
+//    val sum = { x: Int, y: Int -> x + y }
+//    println(sum(1, 2))
+
+//    { println(42) }()
+
+//    run { println(42) }
+
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+    val names = people.joinToString(separator = " ",
+        transform = { p: Person -> p.name })
+    println(names)
+}
